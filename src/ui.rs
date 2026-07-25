acd3cdabc083b1ad5c3b8c2e2286e0ce1d7b80e7
Alexander Mills <alexander.d.mills@gmail.com>
2026-07-25 10:48:47 -0500

    claude autosave 2026-07-25T15:48:47Z

--- src/ui.rs
+++ src/ui.rs
@@ -434,6 +434,7 @@ fn wire_callbacks(app: &AppWindow, state: &Rc<RefCell<AppState>>) {
         let weak = weak.clone();
         app.on_sync_pin_unlock(move |server, pin, passphrase| {
             let Some(app) = weak.upgrade() else { return };
+            note_activity(&state, Interaction::Sync);
             pin_unlock(
                 &app,
                 &state,
